f4a7d71d956e214b35f9211df51b4b4af03b3eb5
Marco Sameh <marcosameh678@gmail.com>
2024-11-19 18:33:57 +0200

    updates

---
+++ AuditLog.DAL/Migrations/20241119163200_logTypeRelation.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Log.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class logTypeRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AuditLogs_ActionTypeId",
+                table: "AuditLogs",
+                column: "ActionTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AuditLogs_ActionTypes_ActionTypeId",
+                table: "AuditLogs",
+                column: "ActionTypeId",
+                principalTable: "ActionTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AuditLogs_ActionTypes_ActionTypeId",
+                table: "AuditLogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AuditLogs_ActionTypeId",
+                table: "AuditLogs");
+        }
+    }
+}
--- AuditLog.Domain/Entities/AuditLogWithEmployee.cs
+++ AuditLog.Domain/Entities/AuditLogWithEmployee.cs
@@ -1,14 +1,14 @@
 ﻿namespace Log.Domain.Entities
 {
-    public class AuditLog
+    public class AuditLogWithEmployee
     {
-        public int Id { get; set; }
-        public int ActionTypeId { get; set; }
+        public int AuditLogId { get; set; }
         public int EmployeeId { get; set; }
+        public string? EmployeeName { get; set; }
+        public int ActionTypeId { get; set; }
+        public string? ActionTypeName { get; set; }
         public DateTime Timestamp { get; set; }
         public string? OldData { get; set; }
         public string? NewData { get; set; }
-        public ActionType ActionType { get; set; }
     }
-
 }
